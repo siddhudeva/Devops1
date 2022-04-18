5ca550587ff11cbf27051767ed1334055ad27596
SiddhuDeva <dsmahadeva8@gmail.com>
2022-04-18 19:59:10 +0530

    bash script

--- shall/git_branch_add.sh
+++ shall/git_branch_add.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [$1 -z ];then
-echo  enter NEW GIT BRANCH NAME along with script ex: git_branch_add.sh <YourBranch>
-exit 1
-fi
+# if [$1 -z ];then
+# echo  enter NEW GIT BRANCH NAME along with script ex: git_branch_add.sh <YourBranch>
+# exit 1
+# fi
 #this script is to validate and creates a new branch
 git branch | grep -w $1
 echo $?

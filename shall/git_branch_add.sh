8a318cba897f25bbfd5fffa88180e56182aaadbe
SiddhuDeva <dsmahadeva8@gmail.com>
2022-04-18 19:56:58 +0530

    bash script

--- shall/git_branch_add.sh
+++ shall/git_branch_add.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
+if [$1 -z ];then
+echo  enter NEW GIT BRANCH NAME along with script ex: git_branch_add.sh <YourBranch>
+exit 1
+fi
 #this script is to validate and creates a new branch
 git branch | grep -w $1
 echo $?
 
 if [$? -eq 0];
 then 
-   this branch is already exists
+   echo this branch is already exists
 else
    git branch $1
 fi

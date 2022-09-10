fc56df986a0e4f1cb89f510eb8936ed7b84bb867
SiddhuDeva <dsmahadeva8@gmail.com>
2022-09-10 22:02:01 +0530

    Using for K8s Practice

--- shall/ping_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# this is to ping all the nodes which are in a given subnet
-echo "*****************Please specify the subnet address upto 3 octacts******************"
-read -p "Please enter here (example : 192.168.0) : " SUBNET
-
-for IP in $(seq 1 254)
-do 
-  ping -c 1 $SUBNET.$IP
-done
